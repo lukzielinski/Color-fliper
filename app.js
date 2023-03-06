77b598fc6f75ec0880eb96eb50acac0d01e45cb6
ReQ <mrzielony22@gmail.com>
2023-03-07 00:14:27 +0100

    Added simple functions

---
+++ app.js
@@ -0,0 +1,13 @@
+const colors = ['lightsalmon', 'mediumspringgreen', 'cornflowerblue', 'gold', 'aquamarine'];
+const btn = document.getElementById('btn'); 
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function() {
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.text = colors[randomNumber];
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
