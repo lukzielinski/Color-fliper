a4ab83efcabaf36ba192721c1c21528068f01c0a
ReQ <mrzielony22@gmail.com>
2023-03-07 00:27:12 +0100

    Added functions to generate hex colors

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const color = document.querySelector('.color');
 btn.addEventListener('click', function() {
     const randomNumber = getRandomNumber();
     document.body.style.backgroundColor = colors[randomNumber];
-    color.text = colors[randomNumber];
+    color.textContent = colors[randomNumber];
 });
 
 function getRandomNumber() {
---
+++ hex.js
@@ -0,0 +1,13 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function() {
+    let hexColor = '#';
+    for(let i = 0; i < 6 ;i++){
+        const getRandomNumber = () => Math.floor(Math.random() * hex.length);
+        hexColor += hex[getRandomNumber()];
+    }
+    document.body.style.backgroundColor = hexColor;
+    color.textContent = hexColor;
+});
